8daa7c20b21ce3186cee0b08d0b95d8c5eca5924
Krzysztof Wisniewski <wisniewskikr@gmail.com>
2014-10-07 15:31:54 +0200

    Final improvement of intg test in porject servlets-example-crud-tests

--- servlets-example-crud-tests/src/main/java/pl/kwi/db/jpa/JpaUtil.java
+++ servlets-example-crud-tests/src/main/java/pl/kwi/db/jpa/JpaUtil.java
@@ -4,10 +4,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class JpaUtil {
 	
 	public static EntityManager createEntityManager(){
-		String persistenceUnitName = System.getProperty("main.db.unit.persistence-unit.name");
+		String persistenceUnitName = System.getProperty("test.db.persistence-unit.name");
+		if(StringUtils.isBlank(persistenceUnitName)){
+			persistenceUnitName = System.getProperty("main.db.unit.persistence-unit.name");
+		}
 		return Persistence.createEntityManagerFactory(persistenceUnitName).createEntityManager();		
 	}
 	
